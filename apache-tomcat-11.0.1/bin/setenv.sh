da413069b9dcff399de003527627342250630bd4
Desesquelles <lilian.desesquelles@etu.inp-n7.fr>
2026-04-07 16:03:28 +0200

    changement comp.sh

---
+++ apache-tomcat-11.0.1/bin/setenv.sh
@@ -0,0 +1,1 @@
+export JAVA_HOME=/home/lilian/Documents/N7/Annee2/s2/Application_web/TP/jdk-17.0.5
--- comp_linux.sh
+++ comp_linux.sh
@@ -1,6 +1,24 @@
+#!/bin/bash
+
+# Build back with Maven
+echo "Building back..."
+cd back/facade
+chmod +x ./mvnw
+./mvnw clean package
+cd ../../
+
+# Build front
+echo "Building front..."
 mkdir -p tmp/WEB_INF/classes
-cp -r $1/bin/pack tmp/WEB_INF/classes/.
-cp -r $1/lib/ tmp/WEB_INF/.
-cp -r $1/src/webcontent/* tmp/.
-jar cf $1.war -C tmp .
+cp -r front/bin/pack tmp/WEB_INF/classes/.
+cp -r front/lib/ tmp/WEB_INF/.
+cp -r front/src/webcontent/* tmp/.
+jar cf front.war -C tmp .
 rm -rf tmp
+
+# Move wars to Tomcat webapps
+echo "Moving wars to Tomcat webapps..."
+cp back/facade/target/back.war apache-tomcat-11.0.1/webapps/back.war
+mv front.war apache-tomcat-11.0.1/webapps/
+
+echo "Build and deployment complete."
